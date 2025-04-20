6d6693969de5c0e05c7523ada97daf660bafa7b1
abc <pas081bct027@wrc.edu.np>
2025-04-20 18:19:58 +0545

    Create hello_world.cpp

---
+++ practice codes/hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+cout << "Hello World!";
+return 0;
+}
+
+/*
+iostream -> The iostream header declares several important objects and classes that you'll encounter frequently
+cout-> character-output 
+<< -> used a bracket 
+*/
