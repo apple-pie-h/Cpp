f13f44d7b16063d021b3fe3e9bf59a0b586216dc
abc <pas081bct027@wrc.edu.np>
2025-04-26 19:30:58 +0545

    Create global_variable.cpp

---
+++ practice codes/global_variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream.h>
+#include<conio.h>
+ int a=10 ; // global a
+void main( )
+{ clrscr( );
+ int a=15 ; // a redeclared, local to main
+cout << “\n Local a=” <<a<<
+ “Global a =” << : : a ;
+ : : a=20 ;
+ cout<< “\n Local a=”<<a
+ << “Global a<<: : a ;
+ getch();
+ }
+
+::
