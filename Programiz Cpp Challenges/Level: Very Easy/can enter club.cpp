c482647dd28ba8e17e65f2ad042667e66658d57b
abc <pas081bct027@wrc.edu.np>
2025-05-12 21:04:08 +0545

    Create can enter club.cpp

---
+++ Programiz Cpp Challenges/Level: Very Easy/can enter club.cpp
@@ -0,0 +1,39 @@
+//programiz compiler 
+
+
+string checkClubEntry(int age) {
+    if(age<21)
+    {
+        return "You cannot enter the club";
+
+    }
+else 
+return "You can enter the club";
+
+}
+
+
+//full program 
+/*
+
+#include<iostream>
+using namespace std;
+string checkClubEntry(int age);
+int main()
+{
+    int age;
+    cout<<"age = ";
+    cin>> age;
+    cout << checkClubEntry(age);
+}
+string checkClubEntry(int age) {
+    if(age<21)
+    {
+        return "You cannot enter the club";
+
+    }
+else 
+return "You can enter the club";
+
+}
+*/
