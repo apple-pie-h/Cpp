9ead06fd3a26a196c3cd7cb72a5066af27adfaa9
abc <pas081bct027@wrc.edu.np>
2025-05-22 07:23:23 +0545

    Create vehicle preference.cpp

---
+++ Programiz Cpp Challenges/Level: Very Easy/vehicle preference.cpp
@@ -0,0 +1,10 @@
+string checkPreference(int preference) {
+    if (preference==2)
+    return "Bike";
+    else if (preference==3)
+    return "Trikes";
+    else if (preference ==4)
+    return "Car";
+    else 
+    return "Other";
+}
