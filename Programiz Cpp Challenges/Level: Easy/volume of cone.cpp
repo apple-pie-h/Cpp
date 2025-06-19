47e114a974f7873dfb6505f3ae00ce9d435c897b
rebika <pas081bct027@wrc.edu.np>
2025-06-19 20:25:34 +0545

    Create volume of cone.cpp

---
+++ Programiz Cpp Challenges/Level: Easy/volume of cone.cpp
@@ -0,0 +1,5 @@
+double calculateConeVolume(double radius, double height) {
+    return ((1.0/3.0)*3.1415*radius*radius*height);
+}
+
+//1.0/3.0 to change the result to double 1/3-> generates 0 and not 0.33
